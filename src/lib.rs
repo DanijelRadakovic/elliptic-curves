ba16560563d6f43bb4341ce8f73d96ff5b0eb51c
DanijelRadakovic <danijelradakovic1996@gmail.com>
2026-07-29 15:27:48 +0200

    tmp

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+pub mod finite_field;
+
+use finite_field::{FiniteField, FiniteFieldError};
 use num_bigint::BigUint;
 use num_iter::range;
 use num_traits::{One, Zero};
@@ -9,9 +12,9 @@ const FOUR: BigUint = BigUint::new_const(4);
 const TWENTY_SEVEN: BigUint = BigUint::new_const(27);
 
 #[derive(Eq, PartialEq, Clone, Debug)]
-pub struct Point {
-    pub x: BigUint,
-    pub y: BigUint,
+pub enum Point {
+    Coordinate { x: BigUint, y: BigUint },
+    Identity,
 }
 
 // E: y^2 = x^3 + ax + b mod p
@@ -31,43 +34,53 @@ pub enum EllipticCurveError {
     PointNotOnCurve { x: BigUint, y: BigUint },
     #[error("Operation {op} failed: {reason}")]
     OperationFailed { op: String, reason: String },
+    #[error(transparent)]
+    FieldError(#[from] FiniteFieldError),
 }
 
 impl EllipticCurve {
     pub fn new(a: BigUint, b: BigUint, p: BigUint) -> Result<Self, EllipticCurveError> {
+        let f = FiniteField { p };
         // Ensure the curve is non-singular: 4a^3 + 27b^2 != 0 (mod p)
-        let a_cubed = a.modpow(&THREE, &p);
-        let b_squared = b.modpow(&TWO, &p);
-
-        let discriminant = (&FOUR * a_cubed + &TWENTY_SEVEN * b_squared) % &p;
+        let a_cubed = f.pow(&THREE, &a);
+        let b_squared = f.pow(&TWO, &b);
+        let discriminant = f.add(&f.mul(&FOUR, &a_cubed), &f.mul(&TWENTY_SEVEN, &b_squared));
 
         if discriminant.is_zero() {
             return Err(EllipticCurveError::InvalidCurve);
         }
 
-        Ok(Self {
-            a,
-            b,
-            f: FiniteField { p },
-        })
+        Ok(Self { a, b, f })
     }
 
     pub fn add(&self, point1: &Point, point2: &Point) -> Result<Point, EllipticCurveError> {
-        let s = self.calculate_slope(point1, point2).map_err(|e| {
-            EllipticCurveError::OperationFailed {
-                op: format!("{:?} + {:?}", point1, point2),
-                reason: e.to_string(),
+        match (point1, point2) {
+            (Point::Identity, Point::Identity) => Ok(Point::Identity),
+            (Point::Identity, p) => Ok(p.clone()),
+            (p, Point::Identity) => Ok(p.clone()),
+            (Point::Coordinate { x: x1, y: y1 }, Point::Coordinate { x: x2, y: y2 }) => {
+                // P + (-P) = Identity
+                if x1 == x2 && y1 != y2 {
+                    return Ok(Point::Identity);
+                }
+
+                let s = self.calculate_slope(point1, point2).map_err(|e| {
+                    EllipticCurveError::OperationFailed {
+                        op: format!("{:?} + {:?}", point1, point2),
+                        reason: e.to_string(),
+                    }
+                })?;
+                // x = s^2 - x1 - x2
+                let x = self
+                    .f
+                    .sub(&self.f.sub(&self.f.pow(&TWO, &s), &x1), &x2);
+                // y = s (x1 - x) - y1
+                let y = self
+                    .f
+                    .sub(&self.f.mul(&s, &self.f.sub(&x1, &x)), &y1);
+                Ok(Point::Coordinate { x, y })
             }
-        })?;
-        // x = s^2 - x1 - x2
-        let x = self
-            .f
-            .sub(&self.f.sub(&self.f.pow(&TWO, &s), &point1.x), &point2.x);
-        // y = s (x1 - x) - y1
-        let y = self
-            .f
-            .sub(&self.f.mul(&s, &self.f.sub(&point1.x, &x)), &point1.y);
-        Ok(Point { x, y })
+        }
     }
 
     // TODO: Improve with bitwise operations
@@ -83,87 +96,39 @@ impl EllipticCurve {
         Ok(result)
     }
 
-    fn calculate_slope(&self, point1: &Point, point2: &Point) -> Result<BigUint, FiniteFieldError> {
-        let s: BigUint;
-        let numerator: BigUint;
-        let denominator: BigUint;
+    pub fn is_on_curve(&self, point: &Point) -> bool {
+        match point {
+            Point::Coordinate { x, y } => {
+                let x_cubed = self.f.pow(&THREE, &x);
+                let a_x = self.f.mul(&self.a, &x);
+                self.f.pow(&TWO, y) == self.f.add(&self.f.add(&x_cubed, &a_x), &self.b)
+            }
+            Point::Identity => true
+        }
+    }
+
+    fn calculate_slope(&self, point1: &Point, point2: &Point) -> Result<BigUint, EllipticCurveError> {
         let f = &self.f;
-        if point1 == point2 {
-            // (3 * x^2 + a) % p
-            numerator = f.add(&f.mul(&THREE, &f.pow(&TWO, &point1.x)), &self.a);
-            // (2 * y) % p
-            denominator = f.mul(&TWO, &point1.y);
+        match (point1, point2) {
+            (Point::Coordinate { x: x1, y: y1 }, Point::Coordinate { x: x2, y: y2 }) => {
+                let (numerator, denominator) = if point1 == point2 {
+                    (
+                        f.add(&f.mul(&THREE, &f.pow(&TWO, &x1)), &self.a),
+                        f.mul(&TWO, &y1)
+                    )
+                } else {
+                    (f.sub(&y2, &y1), f.sub(&x2, &x1))
+                };
 
-            let inv_denom = f.mul_inv(&denominator)?;
-            s = f.mul(&numerator, &inv_denom);
-            Ok(s)
-        } else {
-            numerator = &point2.y - &point1.y;
-            denominator = &point2.x - &point1.x;
-            let inv_denom = f.mul_inv(&denominator)?;
-            s = f.mul(&numerator, &inv_denom);
-            Ok(s)
+                let inv_denom = f.mul_inv(&denominator)?;
+                let s = f.mul(&numerator, &inv_denom);
+                Ok(s)
+            }
+            _ => Err(EllipticCurveError::OperationFailed {
+                op: "calculating slope".into(),
+                reason: "Cannot calculate slope for Identity point".into()
+            })
         }
-    }
-}
-
-pub struct FiniteField {
-    pub p: BigUint,
-}
-
-#[derive(Error, Debug, PartialEq)]
-pub enum FiniteFieldError {
-    #[error("Cannot divide by zero in a finite field")]
-    DivisionByZero,
-    #[error("Zero does not have modular inverse")]
-    ZeroHasNoInverse,
-    #[error("Modular inverse does not exist (modulus might not be prime)")]
-    NoInverse,
-}
-
-impl FiniteField {
-    pub fn add(&self, a: &BigUint, b: &BigUint) -> BigUint {
-        (a + b) % &self.p
-    }
-
-    pub fn sub(&self, a: &BigUint, b: &BigUint) -> BigUint {
-        // Easy implementation: add(a, add_inv(b)).
-        // Improved implementation: a -b = ((a + p - (b mod p)) mod p
-        (a + &self.p - (b % &self.p)) % &self.p
-    }
-
-    pub fn add_inv(&self, a: &BigUint) -> BigUint {
-        // a + x = 0 mod p --> x = p - a
-        // There are cases that needs to be aware of:
-        // 1. a = 0, p - 0 = p, but should be 0.
-        // 2. if a > p, substraction will crash.
-
-        // 1. (a % p) ensures the number is within the field range, handles if a > p.
-        // 2. (p - (a % p)) finds the difference
-        // 3. The final % p ensures that if the result is p, it becomes 0
-        (&self.p - (a % &self.p)) % &self.p
-    }
-
-    pub fn mul(&self, a: &BigUint, b: &BigUint) -> BigUint {
-        (a * b) % &self.p
-    }
-
-    pub fn div(&self, a: &BigUint, b: &BigUint) -> Result<BigUint, FiniteFieldError> {
-        if b.is_zero() {
-            return Err(FiniteFieldError::DivisionByZero);
-        }
-        Ok((a * self.mul_inv(b)?) % &self.p)
-    }
-
-    pub fn pow(&self, n: &BigUint, a: &BigUint) -> BigUint {
-        a.modpow(n, &self.p)
-    }
-
-    pub fn mul_inv(&self, a: &BigUint) -> Result<BigUint, FiniteFieldError> {
-        if a.is_zero() {
-            return Err(FiniteFieldError::ZeroHasNoInverse);
-        }
-        a.modinv(&self.p).ok_or(FiniteFieldError::NoInverse)
     }
 }
 
@@ -180,15 +145,21 @@ mod tests {
                 p: BigUint::from(17u32),
             },
         };
-        let p1 = Point {
+        let p1 = Point::Coordinate {
             x: BigUint::from(5u32),
             y: BigUint::from(1u32),
         };
-        let p2 = Point {
+        let p2 = Point::Coordinate {
             x: BigUint::from(3u32),
             y: BigUint::from(1u32),
         };
         let res = curve.add(&p1, &p2).unwrap();
-        println!("Result: x={}, y={}", res.x, res.y);
+        assert_eq!(
+            res,
+            Point::Coordinate {
+                x: BigUint::from(9u32),
+                y: BigUint::from(16u32)
+            }
+        );
     }
 }
